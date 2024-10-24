84d80ce933b738f5bd69d98f79935e22a30cf181
miscitaofvh <nguyenvanhung14112005@gmail.com>
2024-10-24 20:22:21 +0700

    push source, flowchart, test b2

---
+++ Lab2/btvn2.cpp
@@ -0,0 +1,84 @@
+#include <iostream> 
+#include <vector> 
+#include <algorithm>
+
+using namespace std;
+
+class PhanSo
+{
+private:
+    int tu, mau;
+public:
+    PhanSo(int _tu = 0, int _mau = 1)
+    {
+        tu = _tu;
+        mau = _mau;
+
+        if (mau == 0) mau = 1;
+    }
+    
+    // Dinh nghia >> de nhap phan so
+    friend istream& operator>>(istream& inp, PhanSo& u)
+    {
+        cout << "Nhap tu so: ";
+        inp >> u.tu;
+        cout << "Nhap mau so: ";
+        inp >> u.mau;
+
+        while (u.mau == 0)
+        {
+            cout << "Mau so cua phan so khong the bang 0. Nhap lai: ";
+            inp >> u.mau;
+        }
+
+        return inp;
+    }
+
+    // Dinh nghia << de xuat ra phan so 
+    friend ostream& operator<<(ostream& out, const PhanSo& u) 
+    {
+        out << u.tu << '/' << u.mau << endl;
+        return out;  
+    }
+
+    // Dinh nghia dau be hon
+    bool operator < (const PhanSo& u) const
+    {
+        return tu * u.mau < mau * u.tu;
+    }
+
+    // Dinh nghia dau lon hon
+    bool operator > (const PhanSo& u) const
+    {
+        return tu * u.mau > mau * u.tu;
+    }
+};
+
+vector<PhanSo> a;
+int n, k;
+
+int main()
+{
+    cout << "Nhap so luong phan so: "; 
+    cin >> n;
+    cout << "Nhap so k: ";
+    cin >> k;
+
+    a.resize(n);
+
+    for (int i = 0; i < n; ++i) 
+    {
+        cout << "Nhap phan so thu " << i + 1 << ": " << endl;
+        cin >> a[i];
+    }
+    
+    if (k <= n)
+    {
+        sort(a.begin(), a.end());
+        reverse(a.begin(), a.end());
+        cout << "Phan so lon thu " << k << " la: " << a[k - 1];
+        cout << "Phan so nho thu " << k << " la: " << a[n - k];
+    }
+
+    return 0;
+}
